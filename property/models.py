74402089618efc50a96b55bee25baf5f896bac88
grepto <grepto@gmail.com>
2019-08-15 13:30:23 +0300

    add owner_phone_pure to Flat model

--- property/models.py
+++ property/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
+    owner_phone_pure = PhoneNumberField('Нормализованный номер владельца', blank=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField('Когда создано объявление', default=timezone.now, db_index=True)
 
@@ -17,24 +20,15 @@ class Flat(models.Model):
     address = models.TextField('Адрес квартиры', help_text='ул. Подольских курсантов д.5 кв.4')
     floor = models.CharField('Этаж', max_length=3, help_text='Первый этаж, последний этаж, пятый этаж')
 
-
     rooms_number = models.IntegerField('Количество комнат в квартире', db_index=True)
-
-
-
     living_area = models.IntegerField('количество жилых кв.метров', null=True, db_index=True)
-
     has_balcony = models.NullBooleanField('Наличие балкона', null=True, db_index=True)
-
-
     new_building = models.NullBooleanField('Новостройка', null=True, db_index=True)
 
-
     active = models.BooleanField('Активно-ли объявление', db_index=True)
-
     construction_year = models.IntegerField('Год постройки здания', null=True, db_index=True)
 
-    liked_by = models.ManyToManyField(User, related_name='liked_flats', verbose_name='Кто лайкнул', null=True, blank=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_flats', verbose_name='Кто лайкнул', blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
